62aff5eb46891253cbdca135654e5864141d5992
masterchange <2042313869@qq.com>
2024-07-27 11:16:08 +0800

    init

---
+++ Multi_function_secondary_keyboard.ino
@@ -0,0 +1,42 @@
+#include <BleKeyboard.h>
+BleKeyboard blekeyboard("ESP32kEYBOARD","Espressif",50);
+// 设备名，制造商，电量
+const int OutPIN = 0;    // 定义按键端口
+
+void setup()
+{
+    Serial.begin(115200);
+    blekeyboard.begin();
+    Serial.println("蓝牙键盘准备就绪");
+}
+
+void loop()
+{
+    if(blekeyboard.isConnected())    // 判断连接是否成功
+    {
+        static int a = 1;    // 第一次连接上打印连接成功
+        if(a == 1)
+        {
+            Serial.println("蓝牙已连接");
+            a++;
+        }
+
+        if(!digitalRead(OutPIN))    // 按键被按下
+        {
+            delay(50);
+            if(!digitalRead(OutPIN))    // 消抖，也不知道ESP32上用不用消抖
+            {
+                Serial.println("输出");
+
+                blekeyboard.println("Don't say anything that is not conducive to unity");    // 打印字符
+                delay(100);
+                blekeyboard.write(KEY_NUM_ENTER);    // 按键按下enter
+            }
+        }
+    }
+    else    // 没有连接成功
+    {
+        Serial.println("蓝牙未连接");    // 循环打印蓝牙未连接
+        delay(1000);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Multi_function_secondary_keyboard
+
+## 多功能辅助键盘 该键盘旨在辅助使用电脑，主要提供快捷键
+## 下载地址 https://github.com/masterchange/Multi_function_secondary_keyboard/releases
+
+## 开发平台
+windows 11<br>
+
+## 开发工具
+arduino<br>
+clion<br>
+
+## 开发语言
+c++
