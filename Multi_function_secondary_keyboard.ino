d97c8a5ae20b59c114119e3b7fcf3d4f287b2088
masterchange <2042313869@qq.com>
2024-07-27 12:50:09 +0800

    demo实现

--- Multi_function_secondary_keyboard.ino
+++ Multi_function_secondary_keyboard.ino
@@ -1,42 +1,70 @@
-#include <BleKeyboard.h>
-BleKeyboard blekeyboard("ESP32kEYBOARD","Espressif",50);
-// 设备名，制造商，电量
-const int OutPIN = 0;    // 定义按键端口
+#include "src/BleKeyboard/BleKeyboard.h"
 
-void setup()
-{
+BleKeyboard bleKeyboard("masterchange's Multi-function secondary keyboard", "Espressif", 50);
+
+// 定义行和列引脚
+const int rows[] = {32, 2, 3, 4};
+const int cols[] = {33, 6, 17, 18};
+
+// 定义按键映射
+const uint8_t keymap[4][4] = {
+        {KEY_NUM_0, KEY_F1, KEY_F2, KEY_F3},
+        {KEY_F4, KEY_F5, KEY_F6, KEY_F7},
+        {KEY_F8, KEY_F9, KEY_F10, KEY_F11},
+        {KEY_F12, KEY_F1, KEY_F1, KEY_F1}
+};
+
+short col_size = 4;
+short row_size = 4;
+
+void setup() {
     Serial.begin(115200);
-    blekeyboard.begin();
-    Serial.println("蓝牙键盘准备就绪");
+    Serial.println("Starting BLE Keyboard");
+
+    // 初始化BLE键盘
+    bleKeyboard.begin();
+
+    // 设置行引脚为输入并启用上拉
+    for (int i = 0; i < row_size; i++) {
+        pinMode(rows[i], INPUT_PULLUP);
+    }
+
+    // 设置列引脚为输出并默认高电平
+    for (int i = 0; i < 1; i++) {
+        pinMode(cols[i], OUTPUT);
+        digitalWrite(cols[i], HIGH);
+    }
+
 }
 
-void loop()
-{
-    if(blekeyboard.isConnected())    // 判断连接是否成功
-    {
-        static int a = 1;    // 第一次连接上打印连接成功
-        if(a == 1)
-        {
-            Serial.println("蓝牙已连接");
-            a++;
-        }
+void loop() {
+    if (!bleKeyboard.isConnected()) {
+        Serial.println("BLE Keyboard not connected");
+        delay(1000);
+        return;
+    }
 
-        if(!digitalRead(OutPIN))    // 按键被按下
-        {
-            delay(50);
-            if(!digitalRead(OutPIN))    // 消抖，也不知道ESP32上用不用消抖
-            {
-                Serial.println("输出");
+    Serial.println("i am running");
+    // 扫描矩阵键盘
+    for (int col = 0; col < col_size; col++) {
+        digitalWrite(cols[col], LOW); // 将当前列设置为低电平
 
-                blekeyboard.println("Don't say anything that is not conducive to unity");    // 打印字符
-                delay(100);
-                blekeyboard.write(KEY_NUM_ENTER);    // 按键按下enter
+        for (int row = 0; row < row_size; row++) {
+            if (digitalRead(rows[row]) == LOW) { // 检测按键按下
+                Serial.print("Key pressed: ");
+                Serial.println(keymap[row][col]); // 打印按键值
+                bleKeyboard.press(keymap[row][col]);
+                delay(20); // 简单去抖动处理
+                while (digitalRead(rows[row]) == LOW); // 等待按键释放
+                bleKeyboard.release(keymap[row][col]);
+                Serial.print("Key released: ");
+                Serial.println(keymap[row][col]); // 打印按键释放
+                delay(20); // 简单去抖动处理
             }
         }
+
+        digitalWrite(cols[col], HIGH); // 恢复当前列的高电平
     }
-    else    // 没有连接成功
-    {
-        Serial.println("蓝牙未连接");    // 循环打印蓝牙未连接
-        delay(1000);
-    }
+
+    delay(100); // 合理的扫描间隔
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Multi_function_secondary_keyboard
 
 ## 多功能辅助键盘 该键盘旨在辅助使用电脑，主要提供快捷键
-## 下载地址 https://github.com/masterchange/Multi_function_secondary_keyboard/releases
+## 下载地址 https://github.com/masterchange13/Multi_function_secondary_keyboard
 
 ## 开发平台
 windows 11<br>
@@ -12,3 +12,17 @@ clion<br>
 
 ## 开发语言
 c++
+
+## 键盘功能
+> 1-1 开机
+> 1-2 关机
+> 1-3 重启
+> 1-4 锁屏
+> 2-1 音量加
+> 2-2 音量减
+> 2-3 静音
+> 2-4 music
+> 3-1 下一首
+> 3-2 上一首
+> 3-3 播放/暂停
+> 3-4 
